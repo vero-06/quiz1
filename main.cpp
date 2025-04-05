04fc3f5be20a98cf7d3d88bffc64b7f76e606006
Veronica <vero06@uw.edu>
2025-04-04 21:35:51 -0700

    nonloop and prime number complete

--- main.cpp
+++ main.cpp
@@ -1,28 +1,48 @@
 #include <iostream>
 
-bool is_power_of_2(int);
+bool is_power_of_2_loop(int);
+bool is_power_of_2_nonloop(int);
+bool prime(int);
 
 int main() {
 
     //PROBLEM 1 PART 1: power of 2 loop
     int num;
-    std::cout << "Enter a number: ";
+    std::cout << "Enter a number to see if it is a power of 2: ";
     std::cin >> num;
-    bool result = is_power_of_2(num);
+    bool result = is_power_of_2_loop(num);
     if (result == true) {
-        printf("%d is a power of 2", num);
+        printf("%d is a power of 2 (loop method used)\n", num);
     } else {
-        printf("%d is not a power of 2", num);
+        printf("%d is not a power of 2 (loop method used)\n", num);
     }
+    //PROBLEM 1 PART 2: power of 2 nonloop
+    bool answer = is_power_of_2_nonloop(num);
+    if (answer == true) {
+        printf("%d is a power of 2 (nonloop method used)\n", num);
+    } else {
+        printf("%d is not a power of 2 (nonloop method used)\n", num);
+    }
+
+    //PROBLEM 2
+    int val;
+    std::cout << "Enter a number to see if it is a prime number: ";
+    std::cin >> val;
+    bool result2 = prime(val);
+    if (result2 == true) {
+        printf("PASS\n", val);
+    } else {
+        printf("FAIL\n", val);
+    }
+
+    //PROBLEM 3
+
+
     return 0;
-
-
-
-
 }
 
 //loop
-bool is_power_of_2(int num) {
+bool is_power_of_2_loop(int num) {
     while (num!=1) {
         if (num%2==0) {
         num = num/2;
@@ -34,3 +54,28 @@ bool is_power_of_2(int num) {
 }
 
 //non-loop
+bool is_power_of_2_nonloop(int num) {
+    if (num==1) {
+        return true;
+    } if (num%2==0) {
+        return is_power_of_2_nonloop(num/2);
+    }
+    return false;
+}
+
+//prime
+bool prime(int num) {
+    if (num==1 || num==2) {
+        return true;
+    } else if (num<=0) {
+        return false;
+    } else {
+        for (int i=2; i<=num; ++i) {
+            if (num%i==0) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
