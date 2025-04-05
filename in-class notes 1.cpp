2bffc879439a2ff9e7808a0c95194933bf35417e
Veronica <vero06@uw.edu>
2025-04-04 21:09:09 -0700

    problem 1 loop

---
+++ in-class notes 1.cpp
@@ -0,0 +1,35 @@
+/*
+one of the most important parts about writing code is making comments for organization purposes
+    - it makes the program more legible for others
+    - it explains what the code does & why
+    - in this class we want to write as FEW comments as possible
+        - only write comments where people might misunderstand your program
+            - if it takes you a long time to do, and you're proud of it, add a comment explaining it
+        - if you use a number in the code, EXPLAIN WHERE THE NUMBER COMES FROM
+code must compile on ALL machines
+code must do what it is supposed to do
+we must test to confirm and prove that the code works
+code must be efficient
+efficiency, readability, function, testing
+use whichever C++ style you want (for variable names, etc.), but use it everywhere
+    - be consistent
+
+Git & GitHub lets us keep track of changes and manage them
+    - you have a way to store a snapshot of your code before making a change
+        - a snapshot/version is called a "commit"
+            - you can write text so you can explain what changes in each commit
+        - a branch is another copy of your folder
+    - Git & GitHub also lets you easily collaborate with others
+Git & GitHub terms
+    - repository or "repo" is a folder
+    - commit is a snapshot of the repo at a certain time
+    - branch is a line of development, made up of a series of commits
+
+connect to GitHub
+commit
+send to GitHub
+commit each change
+
+write code
+    - make sure to comment where the numbers come from
+*/
--- main.cpp
+++ main.cpp
@@ -1,13 +1,36 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+bool is_power_of_2(int);
+
 int main() {
 
+    //PROBLEM 1 PART 1: power of 2 loop
+    int num;
+    std::cout << "Enter a number: ";
+    std::cin >> num;
+    bool result = is_power_of_2(num);
+    if (result == true) {
+        printf("%d is a power of 2", num);
+    } else {
+        printf("%d is not a power of 2", num);
+    }
     return 0;
+
+
+
+
 }
 
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.
+//loop
+bool is_power_of_2(int num) {
+    while (num!=1) {
+        if (num%2==0) {
+        num = num/2;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
+
+//non-loop
